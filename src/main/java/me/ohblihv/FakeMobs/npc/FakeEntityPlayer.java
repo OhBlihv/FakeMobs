366127d92411b52bcc876db5e83df9bcf11ed027
OhBlihv <dev@ohblihv.me>
2018-01-04 16:44:31 +0800

    Spawn players with all skin parts enabled

--- src/main/java/me/ohblihv/FakeMobs/npc/FakeEntityPlayer.java
+++ src/main/java/me/ohblihv/FakeMobs/npc/FakeEntityPlayer.java
@@ -1,10 +1,7 @@
 package me.ohblihv.FakeMobs.npc;
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.server.v1_8_R3.EntityPlayer;
-import net.minecraft.server.v1_8_R3.MinecraftServer;
-import net.minecraft.server.v1_8_R3.PlayerInteractManager;
-import net.minecraft.server.v1_8_R3.WorldServer;
+import net.minecraft.server.v1_8_R3.*;
 
 public class FakeEntityPlayer extends EntityPlayer
 {
@@ -12,6 +9,8 @@ public class FakeEntityPlayer extends EntityPlayer
 	public FakeEntityPlayer(MinecraftServer srv, WorldServer world, GameProfile game, PlayerInteractManager interact)
 	{
 		super(srv, world, game, interact);
+
+		this.datawatcher.watch(10, Byte.MAX_VALUE); //All Skin Parts Enabled*/
 	}
 
 }
